6cae792f93f33815b3369cb11e70492a585793b1
majirky <amajirsky@gmail.com>
2024-06-22 22:21:49 +0200

    fix if p.grad is None

--- grokfast.py
+++ grokfast.py
@@ -14,10 +14,10 @@ def gradfilter_ma(
     trigger: bool = False, # For ablation study.
 ) -> Dict[str, deque]:
     if grads is None:
-        grads = {n: deque(maxlen=window_size) for n, p in m.named_parameters() if p.requires_grad}
+        grads = {n: deque(maxlen=window_size) for n, p in m.named_parameters() if p.requires_grad and p.grad is not None}
 
     for n, p in m.named_parameters():
-        if p.requires_grad:
+        if p.requires_grad and p.grad is not None:
             grads[n].append(p.grad.data.detach()) # .cpu())
 
             # Modify the gradients.
@@ -40,10 +40,10 @@ def gradfilter_ema(
     lamb: float = 2.0,
 ) -> Dict[str, torch.Tensor]:
     if grads is None:
-        grads = {n: p.grad.data.detach() for n, p in m.named_parameters() if p.requires_grad}
+        grads = {n: p.grad.data.detach() for n, p in m.named_parameters() if p.requires_grad and p.grad is not None}
 
     for n, p in m.named_parameters():
-        if p.requires_grad:
+        if p.requires_grad and p.grad is not None:
             grads[n] = grads[n] * alpha + p.grad.data.detach() * (1 - alpha)
             p.grad.data = p.grad.data + grads[n] * lamb
 
